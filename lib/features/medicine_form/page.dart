babd50f4ba780a30927e5ed25bbc8952d908e4f1
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 08:13:53 +0900

    Fix style

--- lib/features/medicine_form/page.dart
+++ lib/features/medicine_form/page.dart
@@ -4,6 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:medicalarm/entity/medicine.dart';
 import 'package:medicalarm/features/medicine_form/components/section.dart';
 import 'package:medicalarm/style/button.dart';
+import 'package:medicalarm/style/color.dart';
 
 class MedicineFormPage extends HookConsumerWidget {
   final Medicine? medicine;
@@ -12,7 +13,8 @@ class MedicineFormPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final medicine = useState(this.medicine);
+    final name = useState(medicine?.name ?? '');
+    final notificationSettings = useState(medicine?.notificationSettings ?? []);
     final primaryColor = Theme.of(context).colorScheme.primary;
 
     return Scaffold(
@@ -26,9 +28,9 @@ class MedicineFormPage extends HookConsumerWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
               child: TextFormField(
-                initialValue: medicine.value?.name,
+                initialValue: name.value,
                 onChanged: (value) {
-                  medicine.value = medicine.value?.copyWith(name: value);
+                  name.value = value;
                 },
                 decoration: const InputDecoration(
                   hintText: '薬の名前',
@@ -39,7 +41,7 @@ class MedicineFormPage extends HookConsumerWidget {
               icon: Icons.schedule,
               text: '服用時刻',
               children: [
-                MedicineNotificationSettingSection(notificationSettings: medicine.value?.notificationSettings ?? []),
+                MedicineNotificationSettingSection(notificationSettings: notificationSettings),
               ],
             ),
           ],
@@ -49,21 +51,63 @@ class MedicineFormPage extends HookConsumerWidget {
   }
 }
 
-class MedicineNotificationSettingSection extends HookConsumerWidget {
-  final List<MedicineNotificationSetting> notificationSettings;
+class MedicineNotificationSettingSection extends StatelessWidget {
+  final ValueNotifier<List<MedicineNotificationSetting>> notificationSettings;
   const MedicineNotificationSettingSection({super.key, required this.notificationSettings});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        for (var notificationSetting in notificationSettings)
-          TextFormField(
-            initialValue: notificationSetting.reminderTime.toTimeString(),
+        for (final notificationSetting in notificationSettings.value) ...[
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.border),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  constraints: const BoxConstraints(
+                    minHeight: 48,
+                    maxWidth: double.infinity,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        notificationSetting.reminderTime.toTimeString(),
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  notificationSettings.value = notificationSettings.value.where((element) => element != notificationSetting).toList();
+                },
+                icon: const Icon(Icons.delete),
+              ),
+            ],
           ),
+          const SizedBox(height: 6),
+        ],
+        const SizedBox(height: 10),
         TextButton.icon(
           onPressed: () {
-            // TODO: 追加ボタンを押したときの処理
+            notificationSettings.value = [
+              ...notificationSettings.value,
+              const MedicineNotificationSetting(
+                reminderTime: MedicineNotificationSettingReminderTime(hour: 10, minute: 00),
+                dosingCount: 1,
+                isEnabled: true,
+                useCriticalAlert: false,
+                doserName: null,
+              ),
+            ];
           },
           icon: const Icon(Icons.add),
           label: const Text('服用時刻を追加', style: TextStyle(fontWeight: FontWeight.bold)),
