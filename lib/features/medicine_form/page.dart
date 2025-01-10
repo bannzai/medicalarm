641a0cb7f954b43ce9250b61de346e774e062c4b
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 07:02:18 +0900

    WIP form

--- lib/features/medicine_form/page.dart
+++ lib/features/medicine_form/page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:medicalarm/entity/medicine.dart';
 import 'package:medicalarm/features/medicine_form/components/section.dart';
+import 'package:medicalarm/style/button.dart';
+import 'package:medicalarm/style/color.dart';
 
 class MedicineFormPage extends HookConsumerWidget {
   final Medicine? medicine;
@@ -17,26 +19,60 @@ class MedicineFormPage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text('Medicine Form'),
       ),
-      body: Column(
-        children: [
-          TextFormField(
-            initialValue: medicine.value?.name,
-            onChanged: (value) {
-              medicine.value = medicine.value?.copyWith(name: value);
-            },
-          ),
-          SectionTitle(
-            icon: const Icon(Icons.medical_information),
-            text: '服用時刻',
-            children: [
-              for (var notificationSetting in medicine.value?.notificationSettings ?? [])
-                TextFormField(
-                  initialValue: notificationSetting.reminderTime.toTimeString(),
-                ),
-            ],
-          ),
-        ],
+      backgroundColor: AppColors.formBackground,
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextFormField(
+              initialValue: medicine.value?.name,
+              onChanged: (value) {
+                medicine.value = medicine.value?.copyWith(name: value);
+              },
+              decoration: const InputDecoration(
+                hintText: '薬の名前',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SectionTitle(
+              icon: const Icon(Icons.medical_information),
+              text: '服用時刻',
+              children: [
+                MedicineNotificationSettingSection(notificationSettings: medicine.value?.notificationSettings ?? []),
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class MedicineNotificationSettingSection extends HookConsumerWidget {
+  final List<MedicineNotificationSetting> notificationSettings;
+  const MedicineNotificationSettingSection({super.key, required this.notificationSettings});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (notificationSettings.isEmpty) {
+      return TextButton.icon(
+        onPressed: () {
+          // TODO: 追加ボタンを押したときの処理
+        },
+        icon: const Icon(Icons.add),
+        label: const Text('服用時刻を追加'),
+        style: secondaryButtonStyle.merge(
+          capsuleButtonStyle,
+        ),
+      );
+    } else {
+      return Column(
+        children: [
+          for (var notificationSetting in notificationSettings)
+            TextFormField(
+              initialValue: notificationSetting.reminderTime.toTimeString(),
+            ),
+        ],
+      );
+    }
   }
 }
