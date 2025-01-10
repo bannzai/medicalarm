152423be51657921cd94651bbeaaa31c606b127e
bannzai <kingkong999yhirose@gmail.com>
2025-01-10 16:26:57 +0900

    Fix

--- lib/entity/app_user.dart
+++ lib/entity/app_user.dart
@@ -6,9 +6,9 @@ part 'app_user.g.dart';
 part 'app_user.freezed.dart';
 
 @freezed
+@JsonSerializable(explicitToJson: true)
 class AppUser with _$AppUser {
   const AppUser._();
-  @JsonSerializable(explicitToJson: true)
   const factory AppUser({
     String? id,
     @Default(10) int remainingFreeAIFunction,
