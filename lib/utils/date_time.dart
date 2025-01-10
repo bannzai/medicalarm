14be2ea4881664084ee292c554fb39be677432cc
bannzai <kingkong999yhirose@gmail.com>
2025-01-10 21:25:21 +0900

    Remove unnecessary code

--- lib/utils/date_time.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class DateTimeFormatter {
-  // 12:20:30
-  static String clock(int hour, minute, second) {
-    final format = NumberFormat("00");
-    return "${format.format(hour)}:${format.format(minute)}:${format.format(second)}";
-  }
-}
