e8d8ba4c4e8a0974a93bacbe788c9d584191fa9b
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 07:01:56 +0900

    Add button style

--- lib/style/button.dart
+++ lib/style/button.dart
@@ -20,3 +20,12 @@ ButtonStyle get textButtonStyle => TextButton.styleFrom(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
       minimumSize: const Size(double.infinity, 48.0),
     );
+
+ButtonStyle get capsuleButtonStyle => TextButton.styleFrom(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+      shape: const StadiumBorder(),
+    );
+
+ButtonStyle get secondaryButtonStyle => ButtonStyle(
+      backgroundColor: WidgetStateProperty.all(Colors.white),
+    );
