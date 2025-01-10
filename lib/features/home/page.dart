f7544c46c2fe744198cc2a55f1f7c3da98d54903
bannzai <kingkong999yhirose@gmail.com>
2025-01-10 21:27:45 +0900

    Fix home page

--- lib/features/home/page.dart
+++ lib/features/home/page.dart
@@ -1,11 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:medicalarm/features/medicals/page.dart';
+import 'package:medicalarm/features/settings/page.dart';
+import 'package:medicalarm/style/color.dart';
+import 'package:medicalarm/utils/analytics/analytics.dart';
+
+enum HomePageTabType { medicals, settings }
+
+extension HomePageTabFunctions on HomePageTabType {
+  String get screenName {
+    switch (this) {
+      case HomePageTabType.medicals:
+        return 'ShoppingListPage';
+      case HomePageTabType.settings:
+        return 'SettingsPage';
+    }
+  }
+}
 
 class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Text('HomePage');
+    final tabIndex = useState(0);
+    final ticker = useSingleTickerProvider();
+    final tabController = useTabController(initialLength: HomePageTabType.values.length, vsync: ticker);
+    tabController.addListener(() {
+      tabIndex.value = tabController.index;
+      _screenTracking(tabController.index);
+    });
+
+    return DefaultTabController(
+      length: HomePageTabType.values.length,
+      child: Scaffold(
+        backgroundColor: AppColors.background,
+        appBar: null,
+        bottomNavigationBar: Ink(
+          child: SafeArea(
+            child: TabBar(
+              controller: tabController,
+              labelColor: AppColors.primary,
+              labelStyle: const TextStyle(fontSize: 12),
+              indicatorColor: Colors.transparent,
+              unselectedLabelColor: TextColor.gray,
+              tabs: const <Tab>[
+                Tab(
+                  text: 'お薬',
+                  icon: Icon(Icons.list_alt_outlined),
+                ),
+                Tab(
+                  text: '設定',
+                  icon: Icon(Icons.settings_outlined),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: tabController,
+          children: const [
+            MedicalsPage(),
+            // ECItemsPage(),
+            SettingPage(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _screenTracking(int index) {
+    analytics.setCurrentScreen(
+      screenName: HomePageTabType.values[index].screenName,
+    );
   }
 }
