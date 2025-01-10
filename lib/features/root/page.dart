8de2098a41c53700bd984ee4c07e0383a106c190
bannzai <kingkong999yhirose@gmail.com>
2025-01-10 16:18:35 +0900

    Copy from replai for features/root and features/resolver

---
+++ lib/features/root/page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:medicalarm/features/home/page.dart';
+import 'package:medicalarm/features/localization/resolver.dart';
+import 'package:medicalarm/features/resolver/app_resolvers.dart';
+import 'package:medicalarm/features/resolver/app_user_create.dart';
+import 'package:medicalarm/features/resolver/force_update.dart';
+import 'package:medicalarm/features/resolver/purchase_setup.dart';
+
+class RootPage extends HookConsumerWidget {
+  const RootPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AppLocalizationResolver(builder: (context) {
+      return AppResolvers(builder: (context, user) {
+        return AppUserCreateResolver(builder: (context) {
+          return ForceUpdateResolver(builder: (context) {
+            return PurchaseSetupResolver(
+              userID: user.uid,
+              builder: (context) {
+                return const HomePage();
+              },
+            );
+          });
+        });
+      });
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:replai/features/root/page.dart';
+import 'package:medicalarm/features/root/page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
