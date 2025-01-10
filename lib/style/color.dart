30790d447fad0f52954cbce63b126b0b3a6864f2
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 07:01:46 +0900

    fix color

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
+// Ref: https://saruwakakun.com/design/gallery/palette
 abstract class AppColors {
-  static const Color primary = Colors.pinkAccent;
-  static const Color secondary = Color(0xFF6200EE);
-  static const Color background = Color(0xFF000000);
+  static const Color primary = Color(0xFFEE817B);
+  static const Color secondary = Color(0xFFF6AFAA);
+  static const Color background = Color(0xFFFCECEA);
+  static const Color formBackground = AppColors.background;
   static const Color border = Color(0xFFE0E0E0);
 }
 
 abstract class TextColor {
-  static const Color primary = Colors.purple;
+  static const Color main = AppColors.primary;
+  static const Color gray = Color(0xFF928484);
   static const Color black = Colors.black;
   static const Color white = Colors.white;
-  static const Color darkGray = Color(0xFF49454F);
-  static const Color gray = Color(0xFF7E7E7E);
-  static const Color lightGray = Color(0xFFB1B1B1);
-  static const Color main = Colors.black87;
   static const Color link = Colors.blueAccent;
   static const Color danger = Color(0xFFB00020);
   static const Color discount = Color(0xFFB00020);
