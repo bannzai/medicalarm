8cdafe9ae94341b8ba95ec7f78c5739a37cb242a
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 22:56:26 +0900

    Fix

--- lib/features/medicine_form/components/additional_info/section.dart
+++ lib/features/medicine_form/components/additional_info/section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medicalarm/components/picker/number.dart';
 import 'package:medicalarm/entity/medicine.dart';
 import 'package:medicalarm/features/dose_receiver_form/page.dart';
 import 'package:medicalarm/features/medicine_form/components/section_layout.dart';
@@ -28,7 +29,7 @@ class MedicineAdditionalInfoSection extends StatelessWidget {
         MedicineMemoRow(memo: memo, memoImageURL: memoImageURL),
         MedicineDoseReceiverTile(doseReceiver: doseReceiver),
         MedicineUnitTextField(unit: unit),
-        MedicineStockTextField(stock: stock),
+        MedicineStockTile(unit: unit.value, stock: stock),
       ],
     );
   }
@@ -94,6 +95,52 @@ class MedicineDoseReceiverTile extends StatelessWidget {
               ),
             ),
           );
+        },
+      ),
+    );
+  }
+}
+
+class MedicineUnitTextField extends StatelessWidget {
+  final ValueNotifier<String> unit;
+  const MedicineUnitTextField({super.key, required this.unit});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      initialValue: unit.value,
+      onChanged: (value) {
+        unit.value = value;
+      },
+      decoration: const InputDecoration(
+        hintText: '単位',
+      ),
+    );
+  }
+}
+
+class MedicineStockTile extends StatelessWidget {
+  final String unit;
+  final ValueNotifier<int> stock;
+  const MedicineStockTile({super.key, required this.unit, required this.stock});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
+      child: ListTile(
+        title: unit.isEmpty ? const Text('在庫') : Text('在庫($unit)'),
+        trailing: Wrap(
+          children: [
+            Text(stock.value.toString()),
+            const Icon(Icons.chevron_right),
+          ],
+        ),
+        onTap: () async {
+          final result = await showAppNumberPicker(context, initialNumber: stock.value);
+          if (result != null) {
+            stock.value = result;
+          }
         },
       ),
     );
