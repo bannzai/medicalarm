4859f7c696eb574f0b64b89e65892e5eb2d76717
bannzai <kingkong999yhirose@gmail.com>
2025-01-12 06:54:41 +0900

    Fix cloud storage

--- lib/utils/storage/firebase_cloud_storage.dart
+++ lib/utils/storage/firebase_cloud_storage.dart
@@ -1,15 +1,15 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:uuid/uuid.dart';
 
 final storage = FirebaseStorage.instance;
 final rootRef = storage.ref();
-final chartPartnersRef = rootRef.child('chat_partners');
+
+Reference userRef = rootRef.child('users');
+Reference medicinesRef({required String userID}) => userRef.child(userID).child('medicines');
 
 Future<String> uploadImage(Reference ref, File file) async {
-  final uuid = const Uuid().v4();
-  final uploadTask = ref.child(uuid).putFile(file);
+  final uploadTask = ref.putFile(file);
   final snapshot = await uploadTask.whenComplete(() => null);
   final url = await snapshot.ref.getDownloadURL();
   return url;
