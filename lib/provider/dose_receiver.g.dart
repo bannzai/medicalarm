4a30b54f4c69592732949c5485d412787c9bc0cc
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 22:42:28 +0900

    Add dose-receiver provider

---
+++ lib/provider/dose_receiver.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dose_receiver.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$doseReceiversHash() => r'bc4a5566bc53494fc386b8e5fe0564252e2dc9d6';
+
+/// See also [doseReceivers].
+@ProviderFor(doseReceivers)
+final doseReceiversProvider = AutoDisposeStreamProvider<List<DoseReceiver>>.internal(
+  doseReceivers,
+  name: r'doseReceiversProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$doseReceiversHash,
+  dependencies: <ProviderOrFamily>[userDatabaseProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{userDatabaseProvider, ...?userDatabaseProvider.allTransitiveDependencies},
+);
+
+typedef DoseReceiversRef = AutoDisposeStreamProviderRef<List<DoseReceiver>>;
+String _$doseReceiverAddHash() => r'73ac36014c5c0437977101d0a18ef136bfbd3b08';
+
+/// See also [doseReceiverAdd].
+@ProviderFor(doseReceiverAdd)
+final doseReceiverAddProvider = AutoDisposeProvider<DoseReceiverAdd>.internal(
+  doseReceiverAdd,
+  name: r'doseReceiverAddProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$doseReceiverAddHash,
+  dependencies: <ProviderOrFamily>[userDatabaseProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{userDatabaseProvider, ...?userDatabaseProvider.allTransitiveDependencies},
+);
+
+typedef DoseReceiverAddRef = AutoDisposeProviderRef<DoseReceiverAdd>;
+String _$doseReceiverUpdateHash() => r'f3adc895dfec5d1e814131571096ec1c893e4fda';
+
+/// See also [doseReceiverUpdate].
+@ProviderFor(doseReceiverUpdate)
+final doseReceiverUpdateProvider = AutoDisposeProvider<DoseReceiverUpdate>.internal(
+  doseReceiverUpdate,
+  name: r'doseReceiverUpdateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$doseReceiverUpdateHash,
+  dependencies: <ProviderOrFamily>[userDatabaseProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{userDatabaseProvider, ...?userDatabaseProvider.allTransitiveDependencies},
+);
+
+typedef DoseReceiverUpdateRef = AutoDisposeProviderRef<DoseReceiverUpdate>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
