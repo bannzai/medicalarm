32a5a5ae7d141c011826844aa9a179652aacb180
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 19:53:19 +0900

    Add weekday utils

---
+++ lib/utils/date_time/weekday.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:io';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:intl/intl.dart';
+
+enum Weekday {
+  @JsonKey(name: 'Sunday')
+  Sunday,
+  @JsonKey(name: 'Monday')
+  Monday,
+  @JsonKey(name: 'Tuesday')
+  Tuesday,
+  @JsonKey(name: 'Wednesday')
+  Wednesday,
+  @JsonKey(name: 'Thursday')
+  Thursday,
+  @JsonKey(name: 'Friday')
+  Friday,
+  @JsonKey(name: 'Saturday')
+  Saturday,
+}
+
+extension WeekdayFunctions on Weekday {
+  static Weekday weekdayFromDate(DateTime date) {
+    var weekdayIndex = date.weekday;
+    var weekdays = Weekday.values;
+    var sunday = weekdays.first;
+    weekdays = weekdays.sublist(1)
+      ..addAll(weekdays.sublist(0, weekdays.length))
+      ..insert(0, sunday);
+    return weekdays[weekdayIndex];
+  }
+
+  static List<Weekday> weekdaysForFirstWeekday(Weekday firstWeekday) {
+    return Weekday.values.sublist(firstWeekday.index)..addAll(Weekday.values.sublist(0, firstWeekday.index));
+  }
+
+  static List<String> weekdays() {
+    return DateFormat.EEEE(Platform.localeName).dateSymbols.WEEKDAYS;
+  }
+
+  String weekdayString() {
+    return weekdays()[index];
+  }
+
+  // [日月火水木金土]
+  static List<String> shortWeekdays() {
+    return DateFormat.E(Platform.localeName).dateSymbols.SHORTWEEKDAYS;
+  }
+
+  String weekdayShortString() {
+    return shortWeekdays()[index];
+  }
+}
