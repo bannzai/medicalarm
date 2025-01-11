b36460d40bcd8a5b8fbe414e24ca32c5570e56da
bannzai <kingkong999yhirose@gmail.com>
2025-01-11 16:56:53 +0900

    Fix listtile

--- lib/features/medication_frequency_form/page.dart
+++ lib/features/medication_frequency_form/page.dart
@@ -1,46 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:medicalarm/entity/medication_frequency.dart';
+import 'package:medicalarm/style/button.dart';
 import 'package:medicalarm/theme/form.dart';
 
-class MedicationFrequencyFormPage extends StatelessWidget {
+class MedicationFrequencyFormPage extends HookConsumerWidget {
   final ValueNotifier<MedicationFrequency> frequency;
   const MedicationFrequencyFormPage({super.key, required this.frequency});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final frequency = useState(this.frequency.value);
+    final frequencyValue = frequency.value;
+
     return FormTheme(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('頻度'),
-        ),
         body: SafeArea(
-          child: Column(
-            children: [
-              ListTile(
-                title: const Text('毎日'),
-                trailing: frequency.value is DailyMedicationFrequency ? const Icon(Icons.check) : null,
-              ),
-              ListTile(
-                title: const Text('X日ごと'),
-                trailing: frequency.value is EveryXDaysMedicationFrequency ? const Icon(Icons.check) : null,
-              ),
-              ListTile(
-                title: const Text('特定の曜日'),
-                trailing: frequency.value is SpecificDayOfWeekMedicationFrequency ? const Icon(Icons.check) : null,
-              ),
-              ListTile(
-                title: const Text('月の特定日'),
-                trailing: frequency.value is SpecificDayOfMonthMedicationFrequency ? const Icon(Icons.check) : null,
-              ),
-              ListTile(
-                title: const Text('奇数日/偶数日'),
-                trailing: frequency.value is OddOrEvenDayMedicationFrequency ? const Icon(Icons.check) : null,
-              ),
-              ListTile(
-                title: const Text('周期'),
-                trailing: frequency.value is CycleMedicationFrequency ? const Icon(Icons.check) : null,
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    ListTile(
+                      title: const Text('毎日'),
+                      trailing: frequency.value is DailyMedicationFrequency ? const Icon(Icons.check) : null,
+                      onTap: () {
+                        frequency.value = const MedicationFrequency.daily();
+                      },
+                    ),
+                    const Divider(color: Colors.black, height: 1),
+                    ListTile(
+                      title: const Text('X日ごと'),
+                      trailing: frequency.value is EveryXDaysMedicationFrequency ? const Icon(Icons.check) : null,
+                      onTap: () {
+                        frequency.value = const MedicationFrequency.everyXDays(interval: 1);
+                      },
+                    ),
+                    const Divider(color: Colors.black, height: 1),
+                    ListTile(
+                      title: const Text('特定の曜日'),
+                      trailing: frequency.value is SpecificDayOfWeekMedicationFrequency ? const Icon(Icons.check) : null,
+                      onTap: () {
+                        frequency.value = const MedicationFrequency.specificDayOfWeek(daysOfWeek: [1, 2, 3, 4, 5, 6, 7]);
+                      },
+                    ),
+                    const Divider(color: Colors.black, height: 1),
+                    ListTile(
+                      title: const Text('月の特定日'),
+                      trailing: frequency.value is SpecificDayOfMonthMedicationFrequency ? const Icon(Icons.check) : null,
+                      onTap: () {
+                        frequency.value = const MedicationFrequency.specificDayOfMonth(daysOfMonth: [1, 2, 3, 4, 5, 6, 7]);
+                      },
+                    ),
+                    const Divider(color: Colors.black, height: 1),
+                    ListTile(
+                      title: const Text('奇数日/偶数日'),
+                      trailing: frequency.value is OddOrEvenDayMedicationFrequency ? const Icon(Icons.check) : null,
+                      onTap: () {
+                        frequency.value = const MedicationFrequency.oddOrEvenDay(isOddDay: true);
+                      },
+                    ),
+                    const Divider(color: Colors.black, height: 1),
+                    ListTile(
+                      title: const Text('周期'),
+                      trailing: frequency.value is CycleMedicationFrequency ? const Icon(Icons.check) : null,
+                      onTap: () {
+                        frequency.value = const MedicationFrequency.cycle(consecutiveDays: 21, restDays: 7);
+                      },
+                    ),
+                    const Divider(color: Colors.black, height: 1),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                Column(
+                  children: switch (frequencyValue) {
+                    DailyMedicationFrequency() => [],
+                    EveryXDaysMedicationFrequency() => [
+                        TextFormField(
+                          initialValue: frequencyValue.interval.toString(),
+                          onChanged: (value) {
+                            frequency.value = frequencyValue.copyWith(interval: int.parse(value));
+                          },
+                        ),
+                      ],
+                    SpecificDayOfWeekMedicationFrequency() => [],
+                    SpecificDayOfMonthMedicationFrequency() => [],
+                    OddOrEvenDayMedicationFrequency() => [],
+                    CycleMedicationFrequency() => [],
+                  },
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: ElevatedButton(
+                    style: elevatedButtonStyle,
+                    onPressed: () {
+                      this.frequency.value = frequency.value;
+                      Navigator.pop(context);
+                    },
+                    child: const Text('保存'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
