c2f6bebcc89bdcd43203b6378bb62a392bf6d7b9
bannzai <kingkong999yhirose@gmail.com>
2025-01-12 17:09:20 +0900

    Fix

--- lib/features/medications/page.dart
+++ lib/features/medications/page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:medicalarm/entity/dose_receiver.dart';
+import 'package:medicalarm/provider/medicine.dart';
 
-class MedicationsPage extends StatelessWidget {
+class MedicationsPage extends HookConsumerWidget {
   const MedicationsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Text('服薬');
+  Widget build(BuildContext context, WidgetRef ref) {
+    final medicines = ref.watch(activeMedicinesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('服薬'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('服薬'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
