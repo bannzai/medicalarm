eb35371f0d2505ea1d461613c8553a6943842701
bannzai <kingkong999yhirose@gmail.com>
2025-01-12 16:32:39 +0900

    Add note comment

--- lib/entity/dose_receiver.dart
+++ lib/entity/dose_receiver.dart
@@ -5,6 +5,7 @@ import 'package:medicalarm/entity/timestamp.dart';
 part 'dose_receiver.g.dart';
 part 'dose_receiver.freezed.dart';
 
+// [NOTE:DoseReceiver] DoseReceiver は削除不可にする。Medicine と 紐づいている。一覧表示のGroupBy処理時に DBに存在しない場合のことを考えると条件が大変になるからである
 @freezed
 class DoseReceiver with _$DoseReceiver {
   const DoseReceiver._();
