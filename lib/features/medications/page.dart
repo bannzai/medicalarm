34b9f0faeeddbb6d92bcd11f82bc89cae29017f6
bannzai <kingkong999yhirose@gmail.com>
2025-01-13 12:37:43 +0900

    WIP generate page

--- lib/features/medications/page.dart
+++ lib/features/medications/page.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:medicalarm/entity/dose_receiver.dart';
-import 'package:medicalarm/provider/medicine.dart';
+import 'package:medicalarm/entity/medication_history.dart';
 
 class MedicationsPage extends HookConsumerWidget {
   const MedicationsPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final medicines = ref.watch(activeMedicinesProvider);
+    final medicationHistories = ref.watch(medicationHistoriesProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('服薬'),
+        title: const Text('服薬履歴'),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text('服薬'),
-            ],
-          ),
+        child: ListView.builder(
+          itemCount: medicationHistories.length,
+          itemBuilder: (context, index) {
+            final history = medicationHistories[index];
+            final medicine = history.medicine;
+            final schedule = history.action.medicationSchedule;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(medicine.name),
+                  Text(schedule.quantityMemo),
+                  Text(medicine.doseReceiver?.name ?? 'Default Receiver'),
+                  Text(history.createdDateTime?.toString() ?? 'No Date'),
+                  Text('${schedule.hour}:${schedule.minute}'),
+                  Text(history.memo),
+                  if (history.actionKind == MedicationHistoryActionKind.take)
+                    const Text('Action: Take')
+                  else if (history.actionKind == MedicationHistoryActionKind.revert)
+                    const Text('Action: Revert')
+                  else if (history.actionKind == MedicationHistoryActionKind.skip)
+                    const Text('Action: Skip'),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
