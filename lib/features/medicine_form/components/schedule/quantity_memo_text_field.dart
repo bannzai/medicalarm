8a7706bdc4a4a88918542fe1260648bbbe5df3cc
bannzai <kingkong999yhirose@gmail.com>
2025-01-13 23:20:24 +0900

    Fix

--- lib/features/medicine_form/components/schedule/quantity_memo_text_field.dart
+++ lib/features/medicine_form/components/schedule/quantity_memo_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:medicalarm/entity/medicine.dart';
 
@@ -15,9 +16,13 @@ class MedicineScheduleQuantityMemoTextField extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return TextField(
-      controller: TextEditingController(text: schedule.quantityMemo),
+    final quantityMemo = useState(schedule.quantityMemo);
+
+    return TextFormField(
       onChanged: (value) {
+        quantityMemo.value = value;
+      },
+      onFieldSubmitted: (value) {
         final copied = [...schedules.value];
         copied[index] = copied[index].copyWith(quantityMemo: value);
         schedules.value = copied;
