ed57d0beb42bb6de750ab356e63609bb3175ddb8
bannzai <kingkong999yhirose@gmail.com>
2025-01-19 22:31:38 +0900

    Add diary provider

---
+++ lib/provider/diary.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:medicalarm/entity/diary.dart';
+import 'package:medicalarm/entity/dose_receiver.dart';
+import 'package:medicalarm/features/resolver/database.dart';
+import 'package:medicalarm/provider/dose_receiver.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'diary.g.dart';
+
+@Riverpod(dependencies: [userDatabase])
+Stream<List<DoseReceiver>> doseReceivers(DoseReceiversRef ref) {
+  final database = ref.watch(userDatabaseProvider);
+  return database.doseReceiversReference().snapshots().map((event) => event.docs.map((doc) => doc.data()).toList());
+}
+
+@Riverpod(dependencies: [userDatabase])
+Stream<List<Diary>> diariesForDateTimeRange(DiariesForDateTimeRangeRef ref, {required DateTimeRange dateTimeRange}) {
+  final database = ref.watch(userDatabaseProvider);
+  return database
+      .diariesReference()
+      .where(
+        DiaryFirestoreKey.date,
+        isGreaterThanOrEqualTo: dateTimeRange.start,
+        isLessThanOrEqualTo: dateTimeRange.end,
+      )
+      .snapshots()
+      .map((event) => event.docs.map((doc) => doc.data()).toList())
+      .map((diaries) => _sortedDiaries(diaries));
+}
+
+int _sortDiary(Diary a, Diary b) => a.diaryDate.compareTo(b.diaryDate);
+List<Diary> _sortedDiaries(List<Diary> diaries) {
+  diaries.sort(_sortDiary);
+  return diaries;
+}
