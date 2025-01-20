2b0865b1958ba4c0d2c26df33b0d4e91ba523766
bannzai <kingkong999yhirose@gmail.com>
2025-01-20 14:35:37 +0900

    Fix diarypost

--- lib/features/diary_post/page.dart
+++ lib/features/diary_post/page.dart
@@ -1,51 +1,41 @@
-import 'package:async_value_group/async_value_group.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:medicalarm/components/button/buttons.dart';
-import 'package:medicalarm/components/loading/indicator.dart';
-import 'package:medicalarm/components/retry/page.dart';
+import 'package:medicalarm/components/keyboard/toolbar.dart';
 import 'package:medicalarm/entity/diary.dart';
-import 'package:medicalarm/entity/diary_setting.dart';
+import 'package:medicalarm/features/diary_post/components/memo.dart';
 import 'package:medicalarm/features/localization/l.dart';
 import 'package:medicalarm/provider/diary.dart';
-import 'package:medicalarm/provider/diary_setting.dart';
 import 'package:medicalarm/style/color.dart';
+import 'package:medicalarm/theme/form.dart';
 import 'package:medicalarm/utils/analytics/analytics.dart';
 
 class DiaryPostPage extends HookConsumerWidget {
-  final DateTime date;
+  final DateTime diaryDate;
   final Diary? diary;
 
-  const DiaryPostPage(this.date, this.diary, {super.key});
+  const DiaryPostPage({
+    super.key,
+    required this.diaryDate,
+    required this.diary,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final diarySetting = ref.watch(diarySettingProvider);
-
-    return Retry(
-      retry: () => ref.refresh(diarySettingProvider),
-      child: diarySetting.when(
-        data: (diarySetting) => DiaryPostPageBody(
-          date: date,
-          diary: diary,
-          diarySetting: diarySetting,
-        ),
-        error: (error, stackTrace) => RetryPage(
-          exception: error,
-        ),
-        loading: () => const IndicatorPage(),
-      ),
+    return DiaryPostPageBody(
+      diaryDate: diaryDate,
+      diary: diary,
     );
   }
 }
 
 extension DiaryPostPageRoute on DiaryPostPage {
-  static Route<dynamic> route(DateTime date, Diary? diary) {
+  static Route<dynamic> route(DateTime diaryDate, Diary? diary) {
     return MaterialPageRoute(
       settings: const RouteSettings(name: 'DiaryPostPage'),
-      builder: (_) => DiaryPostPage(date, diary),
+      builder: (_) => DiaryPostPage(diaryDate: diaryDate, diary: diary),
       fullscreenDialog: true,
     );
   }
@@ -54,87 +44,82 @@ extension DiaryPostPageRoute on DiaryPostPage {
 class DiaryPostPageBody extends HookConsumerWidget {
   final DateTime diaryDate;
   final Diary? diary;
-  final DiarySetting? diarySetting;
 
   const DiaryPostPageBody({
     super.key,
     required this.diaryDate,
     required this.diary,
-    required this.diarySetting,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final tags = useState(diarySetting?.tags ?? DiarySetting.defaultTags);
-    final memos = useState(diary?.memos ?? []);
+    final focusNode = useFocusNode();
     final memo = useState(diary?.memo ?? '');
     final diaryPost = ref.watch(diaryPostProvider);
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        elevation: 0.0,
-        leading: IconButton(
-          icon: const Icon(Icons.close, color: Colors.black),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        actions: [
-          AlertButton(
-              text: L.save,
-              onPressed: () async {
-                analytics.logEvent(name: 'diary_post_button_tapped');
-
-                final navigator = Navigator.of(context);
-                await diaryPost.call(
-                  diary: diary,
-                  tags: tags.value,
-                  memos: memos.value,
-                  memo: memo.value,
-                );
-
-                navigator.pop();
-              }),
-        ],
-        backgroundColor: Colors.white,
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                children: [
-                  Text(DateFormat(DateFormat.YEAR_MONTH_DAY).format(diaryDate),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                        color: TextColor.main,
-                      )),
-                  const SizedBox(height: 20),
-                  DiaryPostPhysicalCondition(physicalCondition: physicalCondition),
-                  const SizedBox(height: 20),
-                  DiaryPostPhysicalConditionDetails(
-                      user: user, diarySetting: diarySetting, context: context, physicalConditionDetails: physicalConditionDetails),
-                  const SizedBox(height: 20),
-                  DiaryPostSex(sex: sex),
-                  const SizedBox(height: 20),
-                  DiaryPostMemo(textEditingController: memoTextEditingController, focusNode: focusNode),
-                ],
-              ),
+    return FormTheme(
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            elevation: 0.0,
+            leading: IconButton(
+              icon: const Icon(Icons.close, color: Colors.black),
+              onPressed: () => Navigator.of(context).pop(),
             ),
-            if (focusNode.hasPrimaryFocus) ...[
-              KeyboardToolbar(
-                doneButton: AlertButton(
-                  text: L.completed,
+            actions: [
+              AlertButton(
+                  text: L.save,
                   onPressed: () async {
-                    analytics.logEvent(name: 'post_diary_done_button_pressed');
-                    focusNode.unfocus();
-                  },
-                ),
-              ),
+                    analytics.logEvent(name: 'diary_post_button_tapped');
+
+                    final navigator = Navigator.of(context);
+                    await diaryPost.call(
+                      diary: diary,
+                      memo: memo.value,
+                    );
+
+                    navigator.pop();
+                  }),
             ],
-          ],
+            backgroundColor: Colors.white,
+          ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                    children: [
+                      Text(DateFormat(DateFormat.YEAR_MONTH_DAY).format(diaryDate),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            color: TextColor.main,
+                          )),
+                      const SizedBox(height: 20),
+                      DiaryPostMemo(memo: memo, focusNode: focusNode),
+                    ],
+                  ),
+                ),
+                if (focusNode.hasPrimaryFocus) ...[
+                  KeyboardToolbar(
+                    doneButton: AlertButton(
+                      text: L.completed,
+                      onPressed: () async {
+                        analytics.logEvent(name: 'post_diary_done_button_pressed');
+                        focusNode.unfocus();
+                      },
+                    ),
+                  ),
+                ],
+              ],
+            ),
+          ),
         ),
       ),
     );
