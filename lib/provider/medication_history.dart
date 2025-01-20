35d457e5f217d58d2b0323b71200824d1c31254c
bannzai <kingkong999yhirose@gmail.com>
2025-01-20 17:23:28 +0900

    Fix medication history' g

--- lib/provider/medication_history.dart
+++ lib/provider/medication_history.dart
@@ -1,5 +1,7 @@
 import 'package:medicalarm/entity/medication_history.dart';
+import 'package:medicalarm/features/medications/page.dart';
 import 'package:medicalarm/features/resolver/database.dart';
+import 'package:medicalarm/utils/date_time/date_time_ext.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'medication_history.g.dart';
@@ -8,4 +10,18 @@ part 'medication_history.g.dart';
 Stream<List<MedicationHistory>> medicationHistories(MedicationHistoriesRef ref) {
   final database = ref.watch(userDatabaseProvider);
   return database.medicationHistoriesReference().snapshots().map((event) => event.docs.map((doc) => doc.data()).toList());
+}
+
+@Riverpod(dependencies: [userDatabase])
+Stream<List<MedicationHistory>> medicationHistoriesByDate(MedicationHistoriesRef ref, DateTime date) {
+  final database = ref.watch(userDatabaseProvider);
+  return database
+      .medicationHistoriesReference()
+      .where(
+        'recordDate',
+        isGreaterThanOrEqualTo: date.date(),
+        isLessThanOrEqualTo: date.date().add(const Duration(days: 1)),
+      )
+      .snapshots()
+      .map((event) => event.docs.map((doc) => doc.data()).toList());
 }
--- lib/provider/medication_history.g.dart
+++ lib/provider/medication_history.g.dart
@@ -19,5 +19,150 @@ final medicationHistoriesProvider = AutoDisposeStreamProvider<List<MedicationHis
 );
 
 typedef MedicationHistoriesRef = AutoDisposeStreamProviderRef<List<MedicationHistory>>;
+String _$medicationHistoriesByDateHash() => r'95e06b8fe65d359eb88b08ab12e79f46cc327077';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [medicationHistoriesByDate].
+@ProviderFor(medicationHistoriesByDate)
+const medicationHistoriesByDateProvider = MedicationHistoriesByDateFamily();
+
+/// See also [medicationHistoriesByDate].
+class MedicationHistoriesByDateFamily extends Family<AsyncValue<List<MedicationHistory>>> {
+  /// See also [medicationHistoriesByDate].
+  const MedicationHistoriesByDateFamily();
+
+  /// See also [medicationHistoriesByDate].
+  MedicationHistoriesByDateProvider call(
+    DateTime date,
+  ) {
+    return MedicationHistoriesByDateProvider(
+      date,
+    );
+  }
+
+  @override
+  MedicationHistoriesByDateProvider getProviderOverride(
+    covariant MedicationHistoriesByDateProvider provider,
+  ) {
+    return call(
+      provider.date,
+    );
+  }
+
+  static final Iterable<ProviderOrFamily> _dependencies = <ProviderOrFamily>[userDatabaseProvider];
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static final Iterable<ProviderOrFamily> _allTransitiveDependencies = <ProviderOrFamily>{
+    userDatabaseProvider,
+    ...?userDatabaseProvider.allTransitiveDependencies
+  };
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies => _allTransitiveDependencies;
+
+  @override
+  String? get name => r'medicationHistoriesByDateProvider';
+}
+
+/// See also [medicationHistoriesByDate].
+class MedicationHistoriesByDateProvider extends AutoDisposeStreamProvider<List<MedicationHistory>> {
+  /// See also [medicationHistoriesByDate].
+  MedicationHistoriesByDateProvider(
+    DateTime date,
+  ) : this._internal(
+          (ref) => medicationHistoriesByDate(
+            ref as MedicationHistoriesByDateRef,
+            date,
+          ),
+          from: medicationHistoriesByDateProvider,
+          name: r'medicationHistoriesByDateProvider',
+          debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$medicationHistoriesByDateHash,
+          dependencies: MedicationHistoriesByDateFamily._dependencies,
+          allTransitiveDependencies: MedicationHistoriesByDateFamily._allTransitiveDependencies,
+          date: date,
+        );
+
+  MedicationHistoriesByDateProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.date,
+  }) : super.internal();
+
+  final DateTime date;
+
+  @override
+  Override overrideWith(
+    Stream<List<MedicationHistory>> Function(MedicationHistoriesByDateRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: MedicationHistoriesByDateProvider._internal(
+        (ref) => create(ref as MedicationHistoriesByDateRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        date: date,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeStreamProviderElement<List<MedicationHistory>> createElement() {
+    return _MedicationHistoriesByDateProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is MedicationHistoriesByDateProvider && other.date == date;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, date.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin MedicationHistoriesByDateRef on AutoDisposeStreamProviderRef<List<MedicationHistory>> {
+  /// The parameter `date` of this provider.
+  DateTime get date;
+}
+
+class _MedicationHistoriesByDateProviderElement extends AutoDisposeStreamProviderElement<List<MedicationHistory>> with MedicationHistoriesByDateRef {
+  _MedicationHistoriesByDateProviderElement(super.provider);
+
+  @override
+  DateTime get date => (origin as MedicationHistoriesByDateProvider).date;
+}
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
