e03af80b4f105bf5c28197a7e04654e7efdb84bd
bannzai <kingkong999yhirose@gmail.com>
2025-01-21 00:39:42 +0900

    fix import

--- lib/provider/medication_history.dart
+++ lib/provider/medication_history.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:medicalarm/entity/medication_history.dart';
 import 'package:medicalarm/entity/medicine.dart';
 import 'package:medicalarm/features/resolver/database.dart';
