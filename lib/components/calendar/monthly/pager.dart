39088915672a51f020ddbcb8bdce9744e705ac7d
bannzai <kingkong999yhirose@gmail.com>
2025-01-20 14:42:11 +0900

    Fix

--- lib/components/calendar/monthly/pager.dart
+++ lib/components/calendar/monthly/pager.dart
@@ -1,8 +1,11 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:medicalarm/components/calendar/const.dart';
+import 'package:medicalarm/components/calendar/day/tile.dart';
 import 'package:medicalarm/components/calendar/monthly/calendar.dart';
-import 'package:medicalarm/style/color.dart';
+import 'package:medicalarm/components/calendar/weekly/line.dart';
 import 'package:medicalarm/utils/analytics/analytics.dart';
+import 'package:medicalarm/utils/date_time/date_time+.dart';
 
 class MonthCalendarPager extends StatelessWidget {
   const MonthCalendarPager({
@@ -15,7 +18,7 @@ class MonthCalendarPager extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         color: Colors.white,
       ),
       height: CalendarConst.monthlyCalendarHeight,
@@ -25,9 +28,6 @@ class MonthCalendarPager extends StatelessWidget {
           weekCalendarBuilder: (context, diaries, weekDateRange) {
             return CalendarWeekLine(
               dateRange: weekDateRange,
-              calendarMenstruationBandModels: calendarMenstruationBandModels,
-              calendarScheduledMenstruationBandModels: calendarScheduledMenstruationBandModels,
-              calendarNextPillSheetBandModels: calendarNextPillSheetBandModels,
               horizontalPadding: 0,
               day: (context, weekday, date) {
                 if (date.isPreviousMonth(displayedMonth)) {
@@ -39,8 +39,7 @@ class MonthCalendarPager extends StatelessWidget {
                 return CalendarDayTile(
                   weekday: weekday,
                   date: date,
-                  diary: diaries.firstWhereOrNull((e) => isSameDay(e.date, date)),
-                  schedule: schedules.firstWhereOrNull((e) => isSameDay(e.date, date)),
+                  diary: diaries.firstWhereOrNull((e) => isSameDay(e.diaryDate, date)),
                   onTap: (date) {
                     analytics.logEvent(name: 'did_select_day_tile_on_calendar_card');
                     transitionWhenCalendarDayTapped(context, date: date, diaries: diaries, schedules: schedules);
