fed9c637fd4f984e8472b82c176dd469e3779147
bannzai <kingkong999yhirose@gmail.com>
2025-01-20 23:30:27 +0900

    Fix

--- lib/features/resolver/database.dart
+++ lib/features/resolver/database.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_function_declarations_over_variables
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -26,9 +27,14 @@ abstract class _CollectionPath {
   // static String diarySettings(String userID) => "/users/$userID/diarySettings";
 }
 
-@Riverpod(keepAlive: true, dependencies: [])
+@Riverpod(keepAlive: true, dependencies: [firebaseUserChanges])
 UserDatabase userDatabase(UserDatabaseRef ref) {
-  throw UnimplementedError("userDatabase is not implemented");
+  final stream = ref.watch(firebaseUserChangesProvider);
+  final uid = stream.asData?.value?.uid ?? FirebaseAuth.instance.currentUser?.uid;
+  if (uid == null) {
+    throw Exception('User ID is not found');
+  }
+  return UserDatabase(userID: uid);
 }
 
 class UserDatabase {
