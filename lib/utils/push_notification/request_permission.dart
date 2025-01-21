924a0df3f182ae43c5b68e2c46895f0dd6c31424
bannzai <kingkong999yhirose@gmail.com>
2025-01-21 18:11:06 +0900

    Fix

--- lib/utils/push_notification/request_permission.dart
+++ lib/utils/push_notification/request_permission.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:medicalarm/features/resolver/database.dart';
 import 'package:medicalarm/utils/local_notification/client.dart';
