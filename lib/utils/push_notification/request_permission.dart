320fa36f4dbf85550242b0eb8eaa2246a3926b22
bannzai <kingkong999yhirose@gmail.com>
2025-01-25 05:57:43 +0900

    remove unnecessary code

--- lib/utils/push_notification/request_permission.dart
+++ lib/utils/push_notification/request_permission.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:medicalarm/features/resolver/database.dart';
 import 'package:medicalarm/utils/local_notification/client.dart';
@@ -12,15 +11,10 @@ part 'request_permission.g.dart';
 
 Future<void> requestNotificationPermissions(RegisterRemotePushNotificationToken registerRemotePushNotificationToken) async {
   if (Platform.isIOS) {
-    debugPrint('1: ----');
     await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(alert: true, badge: true, sound: true);
-    debugPrint('2: ----');
     await FirebaseMessaging.instance.requestPermission(alert: true, badge: true, sound: true, announcement: true);
-    debugPrint('3: ----');
     await localNotificationService.requestPermission();
-    debugPrint('4: ----');
     final token = await FirebaseMessaging.instance.getToken();
-    debugPrint('5: ----');
     registerRemotePushNotificationToken(token);
   }
 }
