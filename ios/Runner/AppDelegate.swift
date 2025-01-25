52724db85d4e2a7902c806b1246900949a6f05e9
bannzai <kingkong999yhirose@gmail.com>
2025-01-25 17:20:04 +0900

    Add native implementation for AppTrackingTransparency

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,10 +3,28 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private var channel: FlutterMethodChannel
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    channel = FlutterMethodChannel(
+      name: "method.channel.bannzai.medicalarm",
+      binaryMessenger: viewController.binaryMessenger
+    )
+
+    channel?.setMethodCallHandler({ call, _completionHandler in
+      let completionHandler: (Dictionary<String, Any>) -> Void = {
+        _completionHandler($0)
+      }
+
+      switch call.method {
+        case "requestAppTrackingTransparency":
+          requestAppTrackingTransparency(completion: completionHandler)
+      }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
