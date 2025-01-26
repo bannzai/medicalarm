5b05414a828654ed2fa03aed5a657699d9e86915
bannzai <kingkong999yhirose@gmail.com>
2025-01-26 18:06:32 +0900

    Fix

--- lib/features/preium_introduction/components/premium_introduction_header.dart
+++ lib/features/preium_introduction/components/premium_introduction_header.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:medicalarm/style/color.dart';
 
 class PremiumIntroductionHeader extends StatelessWidget {
   const PremiumIntroductionHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text(
-          "Medicalarm",
-          style: TextStyle(color: Colors.black, fontFamily: "NotoSansJP", fontSize: 32, fontWeight: FontWeight.w900),
-        ),
-        Text(
-          "プレミアムプラン",
-          style: TextStyle(color: Colors.black, fontFamily: "NotoSansJP", fontSize: 24, fontWeight: FontWeight.w600),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/premium_header.png",
+            height: 140,
+          ),
+          const Text(
+            "プレミアムプラン",
+            style: TextStyle(color: TextColor.white, fontFamily: "NotoSansJP", fontSize: 24, fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/features/preium_introduction/premium_introduction_sheet.dart
+++ lib/features/preium_introduction/premium_introduction_sheet.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:medicalarm/entity/dose_receiver.dart';
 import 'package:medicalarm/entity/medicine.dart';
+import 'package:medicalarm/style/color.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:medicalarm/components/loading/indicator.dart';
 import 'package:medicalarm/components/retry/page.dart';
@@ -65,139 +66,160 @@ class _Body extends HookConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        leading: const CloseButton(color: Colors.black),
-        title: const PremiumIntroductionHeader(),
-        backgroundColor: Colors.white,
-        toolbarHeight: 100,
+        leading: const SizedBox.shrink(),
+        backgroundColor: AppColors.primary,
+        toolbarHeight: 0,
         elevation: 1,
       ),
       body: Container(
         color: Colors.white,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.only(bottom: 100, top: 20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (customerInfo.activeSubscriptions.isNotEmpty) ...[
-                const SizedBox(height: 32),
-                const PremiumUserThanksRow(),
-              ],
-              if (customerInfo.activeSubscriptions.isEmpty) ...[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      border: Border.all(
-                        width: 0.4,
-                        color: Colors.grey.shade500,
-                      ),
-                    ),
-                    child: DefaultTextStyle(
-                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Row(
-                            children: [
-                              Icon(Icons.remove_red_eye),
-                              SizedBox(width: 8),
-                              Text('広告の非表示'),
-                            ],
-                          ),
-                          const SizedBox(height: 4),
-                          const Row(
-                            children: [
-                              Icon(Icons.history),
-                              SizedBox(width: 8),
-                              Text('服用履歴をすべて表示'),
-                            ],
-                          ),
-                          const SizedBox(height: 4),
-                          Row(
-                            children: [
-                              const Icon(Icons.medication),
-                              const SizedBox(width: 8),
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    const TextSpan(
-                                      text: '薬の登録数を',
-                                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
-                                    ),
-                                    TextSpan(
-                                      text: '${Medicine.maxCount(isPremium: false)} → ${Medicine.maxCount(isPremium: true)}',
-                                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 4),
-                          Row(
-                            children: [
-                              const Icon(Icons.schedule),
-                              const SizedBox(width: 8),
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    const TextSpan(
-                                      text: '通知のスケジュール数を',
-                                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
-                                    ),
-                                    TextSpan(
-                                      text: '${MedicationSchedule.maxCount(isPremium: false)} → ${MedicationSchedule.maxCount(isPremium: true)}',
-                                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 4),
-                          Row(
-                            children: [
-                              const Icon(Icons.person),
-                              const SizedBox(width: 8),
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    const TextSpan(
-                                      text: '服用者の登録数を',
-                                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
-                                    ),
-                                    TextSpan(
-                                      text: '${DoseReceiver.maxCount(isPremium: false)} → ${DoseReceiver.maxCount(isPremium: true)}',
-                                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+        child: Column(
+          children: [
+            Container(
+              color: AppColors.primary,
+              child: const Stack(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: PremiumIntroductionHeader(),
+                  ),
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: CloseButton(
+                      color: Colors.black,
                     ),
                   ),
-                ),
-                const SizedBox(height: 12),
-                PurchaseButtons(
-                  offeringType: offeringType,
-                  monthlyPackage: monthlyPackage,
-                  annualPackage: annualPackage,
-                  isLoading: isLoading,
-                ),
-              ],
-              const SizedBox(height: 24),
-              PremiumIntroductionFotter(
-                isLoading: isLoading,
+                ],
               ),
-            ],
-          ),
+            ),
+            SingleChildScrollView(
+              padding: const EdgeInsets.only(bottom: 100, top: 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (customerInfo.activeSubscriptions.isNotEmpty) ...[
+                    const SizedBox(height: 32),
+                    const PremiumUserThanksRow(),
+                  ],
+                  if (customerInfo.activeSubscriptions.isEmpty) ...[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                          border: Border.all(
+                            width: 0.4,
+                            color: Colors.grey.shade500,
+                          ),
+                        ),
+                        child: DefaultTextStyle(
+                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Row(
+                                children: [
+                                  Icon(Icons.remove_red_eye),
+                                  SizedBox(width: 8),
+                                  Text('広告の非表示'),
+                                ],
+                              ),
+                              const SizedBox(height: 4),
+                              const Row(
+                                children: [
+                                  Icon(Icons.history),
+                                  SizedBox(width: 8),
+                                  Text('服用履歴をすべて表示'),
+                                ],
+                              ),
+                              const SizedBox(height: 4),
+                              Row(
+                                children: [
+                                  const Icon(Icons.medication),
+                                  const SizedBox(width: 8),
+                                  Text.rich(
+                                    TextSpan(
+                                      children: [
+                                        const TextSpan(
+                                          text: '薬の登録数を',
+                                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
+                                        ),
+                                        TextSpan(
+                                          text: '${Medicine.maxCount(isPremium: false)} → ${Medicine.maxCount(isPremium: true)}',
+                                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 4),
+                              Row(
+                                children: [
+                                  const Icon(Icons.schedule),
+                                  const SizedBox(width: 8),
+                                  Text.rich(
+                                    TextSpan(
+                                      children: [
+                                        const TextSpan(
+                                          text: '通知のスケジュール数を',
+                                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
+                                        ),
+                                        TextSpan(
+                                          text: '${MedicationSchedule.maxCount(isPremium: false)} → ${MedicationSchedule.maxCount(isPremium: true)}',
+                                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 4),
+                              Row(
+                                children: [
+                                  const Icon(Icons.person),
+                                  const SizedBox(width: 8),
+                                  Text.rich(
+                                    TextSpan(
+                                      children: [
+                                        const TextSpan(
+                                          text: '服用者の登録数を',
+                                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
+                                        ),
+                                        TextSpan(
+                                          text: '${DoseReceiver.maxCount(isPremium: false)} → ${DoseReceiver.maxCount(isPremium: true)}',
+                                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    PurchaseButtons(
+                      offeringType: offeringType,
+                      monthlyPackage: monthlyPackage,
+                      annualPackage: annualPackage,
+                      isLoading: isLoading,
+                    ),
+                  ],
+                  const SizedBox(height: 24),
+                  PremiumIntroductionFotter(
+                    isLoading: isLoading,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
