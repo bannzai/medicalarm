f3aac6724fb783952bd4942ef48851fe9660b868
bannzai <kingkong999yhirose@gmail.com>
2025-01-28 19:25:31 +0900

    Revert "feat: l10n 対応（プレミアムイントロダクションフッター）"
    This reverts commit 48ebc0a9a4111652b190a27fce8e6fafb4c57c2b.

--- lib/features/preium_introduction/components/premium_introduction_footer.dart
+++ lib/features/preium_introduction/components/premium_introduction_footer.dart
@@ -74,7 +74,7 @@ class PremiumIntroductionFotter extends StatelessWidget {
                     text: '・${L.autoRenewalNotice}\n',
                   ),
                   TextSpan(
-                    text: '・${L.premiumIntroductionFooterDescription(storeName: storeName)}\n',
+                    text: '・購入後、自動更新の解約は${storeName}アプリのアカウント設定で行えます。(アプリ内から自動更新の解約は行なえません)。',
                   ),
                 ],
               ),
