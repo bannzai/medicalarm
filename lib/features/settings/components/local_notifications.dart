2e112163292c997c747fd717cf413bd01dd8c696
bannzai <kingkong999yhirose@gmail.com>
2025-02-14 09:26:25 +0900

    Add local notifications

---
+++ lib/features/settings/components/local_notifications.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:medicalarm/utils/local_notification/client.dart';
+
+class LocalNotifications extends StatelessWidget {
+  const LocalNotifications({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('Local Notifications'),
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => LocalNotificationsPage()));
+      },
+    );
+  }
+}
+
+class LocalNotificationsPage extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final pendingLocalNotifications = useState<List<PendingNotificationRequest>>([]);
+    useEffect(() {
+      void f() async {
+        pendingLocalNotifications.value = await localNotificationService.pendingReminderNotifications();
+      }
+
+      f();
+      return null;
+    }, []);
+
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Text(pendingLocalNotifications.value[index].id.toString());
+      },
+      itemCount: pendingLocalNotifications.value.length,
+    );
+  }
+}
