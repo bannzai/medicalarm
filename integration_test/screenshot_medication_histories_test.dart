2fc264097cfdc948dd9e9fd9038df2835b1a2cbd
bannzai <kingkong999yhirose@gmail.com>
2025-02-14 21:48:04 +0900

    Fix warning

--- integration_test/screenshot_medication_histories_test.dart
+++ integration_test/screenshot_medication_histories_test.dart
@@ -9,13 +9,13 @@ import 'package:medicalarm/provider/shared_preferences.dart';
 import 'package:medicalarm/style/color.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../lib/screenshot/screenshot_medication_histories.dart';
+import 'package:medicalarm/screenshot/screenshot_medication_histories.dart';
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    final locales = AppLocalizations.supportedLocales;
+    const locales = AppLocalizations.supportedLocales;
     for (final locale in locales) {
       testWidgets('screenshot medication histories $locale', (tester) async {
         debugPrint('Intl.defaultLocale: ${Intl.defaultLocale}, locale.languageCode: ${locale.languageCode}');
