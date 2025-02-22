d6965c1c28a0a3c9019eb8710e781960da66bc00
bannzai <kingkong999yhirose@gmail.com>
2025-02-22 14:18:49 +0900

    Fix project dir

--- scripts/translation/generate_metadata.py
+++ scripts/translation/generate_metadata.py
@@ -15,6 +15,8 @@ GENERATE_SCREENSHOT_TEST_FUNCTION_1 = "screenshot_medications"
 GENERATE_SCREENSHOT_TEST_FUNCTION_2 = "screenshot_medication_histories"
 GENERATE_SCREENSHOT_TEST_FUNCTION_3 = "screenshot_medicine_schedule_notification_form"
 
+os.chdir("PROJECT_DIR")
+
 
 def map_directory_name(arg):
     if arg == "ar":
