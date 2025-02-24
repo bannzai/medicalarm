e707dff03a86e70189ab98721d58b773e293b075
bannzai <kingkong999yhirose@gmail.com>
2025-02-25 04:41:29 +0900

    Fix resolvers

--- lib/features/root/page.dart
+++ lib/features/root/page.dart
@@ -3,9 +3,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:medicalarm/features/home/page.dart';
 import 'package:medicalarm/features/localization/resolver.dart';
 import 'package:medicalarm/features/resolver/app_entity_prepare.dart';
-import 'package:medicalarm/features/resolver/app_resolvers.dart';
 import 'package:medicalarm/features/resolver/app_user.dart';
 import 'package:medicalarm/features/resolver/app_user_create.dart';
+import 'package:medicalarm/features/resolver/auth.dart';
+import 'package:medicalarm/features/resolver/database.dart';
 import 'package:medicalarm/features/resolver/force_update.dart';
 import 'package:medicalarm/features/resolver/in_app_review.dart';
 import 'package:medicalarm/features/resolver/purchase_setup.dart';
@@ -17,26 +18,28 @@ class RootPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return AppLocalizationResolver(builder: (context) {
-      return AppResolvers(builder: (context, user) {
-        return AppUserCreateResolver(builder: (context) {
-          return ForceUpdateResolver(builder: (context) {
-            return PurchaseSetupResolver(
-              userID: user.uid,
-              builder: (context) {
-                return AppEntityPrepareResolver(
-                  userID: user.uid,
-                  builder: (context) {
-                    return Stack(
-                      children: [
-                        const InAppReviewResolver(),
-                        AppUserStreamResolver(stream: (user) => analyticsDebugIsEnabled = user.analyticsDebugIsEnabled),
-                        const HomePage(),
-                      ],
-                    );
-                  },
-                );
-              },
-            );
+      return AuthResolver(builder: (_, user) {
+        return UserDatabaseResolver(builder: (context) {
+          return AppUserCreateResolver(builder: (context) {
+            return ForceUpdateResolver(builder: (context) {
+              return PurchaseSetupResolver(
+                userID: user.uid,
+                builder: (context) {
+                  return AppEntityPrepareResolver(
+                    userID: user.uid,
+                    builder: (context) {
+                      return Stack(
+                        children: [
+                          const InAppReviewResolver(),
+                          AppUserStreamResolver(stream: (user) => analyticsDebugIsEnabled = user.analyticsDebugIsEnabled),
+                          const HomePage(),
+                        ],
+                      );
+                    },
+                  );
+                },
+              );
+            });
           });
         });
       });
