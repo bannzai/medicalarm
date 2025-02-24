b57e7e0dd05e093a3b239e7358de2c796f505b77
bannzai <kingkong999yhirose@gmail.com>
2025-02-25 04:15:17 +0900

    Use keychain shared keychain access

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +25,7 @@ void main() async {
       MobileAds.instance.initialize(),
       Firebase.initializeApp(),
     ).wait;
+    FirebaseAuth.instance.setSettings(userAccessGroup: 'TQPN82UBBY.com.bannzai.medicalarm');
 
     // ignore: prefer_typing_uninitialized_variables
     final (_, sharedPreferences, _) = await (
