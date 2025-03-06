f7b6409d52891ac3b15b0a4c5b28ac60829ba0ce
bannzai <kingkong999yhirose@gmail.com>
2025-03-06 12:05:31 +0900

    Fix

--- lib/features/medicine_form/components/schedule/focus_connect/section.dart
+++ lib/features/medicine_form/components/schedule/focus_connect/section.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:medicalarm/entity/medicine.dart';
 import 'package:medicalarm/features/localization/l.dart';
 import 'package:medicalarm/features/medicine_form/components/section_layout.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:uuid/uuid.dart';
+
+const focusConnectAccessToken = 'f6c5cc5d-248c-4ed4-accc-e8a74018d41d';
 
 class MedicineScheduleFocusConnectSettingSection extends HookConsumerWidget {
+  final MedicationSchedule schedule;
   final ValueNotifier<bool> isEnabled;
 
   const MedicineScheduleFocusConnectSettingSection({
     super.key,
+    required this.schedule,
     required this.isEnabled,
   });
 
@@ -17,10 +24,16 @@ class MedicineScheduleFocusConnectSettingSection extends HookConsumerWidget {
       icon: Icons.notifications,
       text: L.notificationSetting,
       children: [
-        SwitchListTile(
-          value: isEnabled.value,
-          onChanged: (value) {
-            isEnabled.value = value;
+        ListTile(
+          onTap: () {
+            final start = '${schedule.hour}:${schedule.minute}:30';
+            final end = '${schedule.hour + 3}:${schedule.minute}:00';
+            final authCode = const Uuid().v4();
+            launchUrl(
+              Uri.parse(
+                'focus-connect://schedule?accessToken=$focusConnectAccessToken&intervalStartTimeOfDay=$start&intervalEndTimeOfDay=$end&repeats=true&authCode=$authCode',
+              ),
+            );
           },
           title: const Text('通知受信から服薬記録するまで他のアプリをブロックする'),
           subtitle: const Text('通知受信から服薬記録するまで他のアプリをブロックする機能が有効になります。'),
