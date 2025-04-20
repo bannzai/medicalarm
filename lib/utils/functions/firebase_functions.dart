9e46c9834b60ddab68d9645dfb7a7a0b21025d1f
bannzai <kingkong999yhirose@gmail.com>
2025-04-20 14:03:54 +0900

    fix firebase_functions

--- lib/utils/functions/firebase_functions.dart
+++ lib/utils/functions/firebase_functions.dart
@@ -1,89 +1,19 @@
 import 'package:cloud_functions/cloud_functions.dart';
-import 'package:flutter/foundation.dart';
-import 'package:replai/entity/chat_message.dart';
-import 'package:replai/entity/chat_partner.dart';
-import 'package:replai/features/chat_messages/components/image_picker_button.dart';
-import 'package:replai/features/chat_partner_form/components/image_picker_button.dart';
 
-// GenKitがus-central1のサポートになるt a
-final functions = FirebaseFunctions.instanceFor(region: 'us-central1');
+final functions = FirebaseFunctions.instanceFor(region: 'asia-northeast1');
 
 extension FirebaseFunctionsExt on FirebaseFunctions {
-  Future<String> chatReview({required UserChatMessage userChatMessage}) async {
-    final result = await httpsCallable('chatReview').call(
+  Future<bool> startPromotion({required int promotionDayCount}) async {
+    final result = await httpsCallable('startPromotion').call(
       {
-        "userChatMessage": {
-          'id': userChatMessage.id,
-          'chatPartnerID': userChatMessage.chatPartnerID,
-        },
-      },
-    );
-    debugPrint("chatReview.result: ${result.data}");
-    final response = mapToJSON(result.data);
-    if (response['result'] != 'OK') {
-      throw Exception(response['error']['message']);
-    }
-    return response['data']['review'] as String;
-  }
-
-  Future<String> generateReply({required ChatPartner chatPartner}) async {
-    final result = await httpsCallable('generateReply').call(
-      {
-        "chatPartner": {
-          'id': chatPartner.id,
-          'nationality': chatPartner.nationality,
-          'biography': chatPartner.biography,
-          'relationshipDetails': chatPartner.relationshipDetails,
-        },
-      },
-    );
-    debugPrint("generateReply.result: ${result.data}");
-    final response = mapToJSON(result.data);
-    if (response['result'] != 'OK') {
-      throw Exception(response['error']['message']);
-    }
-    return response['data']['reply'] as String;
-  }
-
-  Future<ChatPartnerFormInfo> generateChatPartnerFormFromImage({required String mimeType, required String base64Image}) async {
-    final result = await httpsCallable('generateChatPartnerFormFromImage').call(
-      {
-        "mimeType": mimeType,
-        "base64Image": base64Image,
-      },
-    );
-    final response = mapToJSON(result.data);
-
-    if (response['result'] != 'OK') {
-      throw Exception(response['error']['message']);
-    }
-
-    return ChatPartnerFormInfo(
-      name: response['data']['name'] as String,
-      biography: response['data']['biography'] as String,
-      relationshipDetails: response['data']['relationshipDetails'] as String,
-    );
-  }
-
-  Future<List<ChatMessageFormFromImage>> generateChatMessagesFromImage({required String mimeType, required String base64Image}) async {
-    final result = await httpsCallable('generateChatMessagesFromImage').call(
-      {
-        "mimeType": mimeType,
-        "base64Image": base64Image,
+        'promotionDayCount': promotionDayCount,
       },
     );
     final response = mapToJSON(result.data);
     if (response['result'] != 'OK') {
       throw Exception(response['error']['message']);
     }
-
-    final chatMessages = response['data']['chatMessages'] as List<dynamic>;
-    return chatMessages.map((e) {
-      return ChatMessageFormFromImage(
-        isRight: e['isRight'] as bool,
-        message: e['message'] as String,
-      );
-    }).toList();
+    return response['data']['isAlreadyExist'] as bool;
   }
 }
 
