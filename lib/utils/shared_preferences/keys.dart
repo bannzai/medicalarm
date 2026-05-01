e4f694839bbe8ecec01436b80270a88b1351ba4f
bannzai <star.kojiki@gmail.com>
2026-05-02 08:14:00 +0900

    feat(client): submitFeatureRequest 呼び出しと SharedPreferences キーを追加
    - FirebaseFunctionsExt に submitFeatureRequest メソッド追加
    - 起動時ダイアログの再表示抑止用 DoubleKey extension に
      featureRequestPromptShownDateTimeInterval を追加

--- lib/utils/shared_preferences/keys.dart
+++ lib/utils/shared_preferences/keys.dart
@@ -7,3 +7,9 @@ extension StringKey on String {
 extension ReleaseNoteKey on String {}
 
 extension IntKey on String {}
+
+extension DoubleKey on String {
+  /// 起動時の機能要望ダイアログを最後に表示した時刻（epoch 秒）。
+  /// 30日以内の再表示を抑止するために使用。
+  static const String featureRequestPromptShownDateTimeInterval = 'featureRequestPromptShownDateTimeInterval';
+}
