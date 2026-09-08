8ea9e9a397bcd1bfb96070ba0597c0b847c86e05
bannzai <star.kojiki@gmail.com>
2026-09-08 20:28:00 +0900

    Revert "Merge pull request #284 from bannzai/issue-278"
    This reverts commit 84e87bedda6c6ff1c539da9ac39419d1e35b047a, reversing
    changes made to 4f392b2f1f9f8525891a4842345e066887120d70.

--- lib/features/medications/entity/grouped.dart
+++ lib/features/medications/entity/grouped.dart
@@ -1,9 +1,11 @@
 import 'package:collection/collection.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:medicalarm/entity/dose_receiver.dart';
+import 'package:medicalarm/entity/medication_frequency.dart';
 import 'package:medicalarm/entity/medication_history.dart';
 import 'package:medicalarm/entity/medicine.dart';
 import 'package:medicalarm/utils/date_time/date_time_ext.dart';
+import 'package:medicalarm/utils/date_time/weekday.dart';
 
 part 'grouped.freezed.dart';
 
@@ -70,7 +72,18 @@ List<MedicationGroup> medicationGroups({
     if (medicine.beganDateTime.date().isAfter(date.date())) {
       continue;
     }
-    if (!medicine.frequency.isScheduledOnDate(beganDateTime: medicine.beganDateTime, date: date)) {
+    final bool isMatched;
+    switch (medicine.frequency) {
+      case DailyMedicationFrequency():
+        isMatched = true;
+      case EveryXDaysMedicationFrequency(interval: final interval):
+        isMatched = daysBetween(medicine.beganDateTime, date.date()) % interval == 0;
+      case SpecificWeekdaysMedicationFrequency(weekdays: final weekdays):
+        isMatched = weekdays.any((weekday) => WeekdayFunctions.weekdayFromDate(date.date()) == weekday);
+      case CycleMedicationFrequency(consecutiveDays: final consecutiveDays, restDays: final restDays):
+        isMatched = daysBetween(medicine.beganDateTime, date.date()) % (consecutiveDays + restDays) < consecutiveDays;
+    }
+    if (!isMatched) {
       continue;
     }
 
@@ -106,7 +119,18 @@ List<MedicationGroup> medicationGroups({
     if (medicine.beganDateTime.date().isAfter(date.date())) {
       continue;
     }
-    if (!medicine.frequency.isScheduledOnDate(beganDateTime: medicine.beganDateTime, date: date)) {
+    final bool isMatched;
+    switch (medicine.frequency) {
+      case DailyMedicationFrequency():
+        isMatched = true;
+      case EveryXDaysMedicationFrequency(interval: final interval):
+        isMatched = daysBetween(medicine.beganDateTime, date.date()) % interval == 0;
+      case SpecificWeekdaysMedicationFrequency(weekdays: final weekdays):
+        isMatched = weekdays.any((weekday) => WeekdayFunctions.weekdayFromDate(date.date()) == weekday);
+      case CycleMedicationFrequency(consecutiveDays: final consecutiveDays, restDays: final restDays):
+        isMatched = daysBetween(medicine.beganDateTime, date.date()) % (consecutiveDays + restDays) < consecutiveDays;
+    }
+    if (!isMatched) {
       continue;
     }
 
